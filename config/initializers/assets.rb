2287ff79a267a40f06f4deaa858a6d50924d981f
akshay <velpulaakshaypaul@tamu.edu>
2015-12-05 18:27:57 -0600

    added config/assets/paths

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += %w( style.css )
 Rails.application.config.assets.precompile += %w( login.css )
 Rails.application.config.assets.precompile += %w( depot.css )
 Rails.application.config.assets.precompile += %w( jquery-ui.js )
+Rails.application.config.assets.precompile += %w( jquery-ui.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
